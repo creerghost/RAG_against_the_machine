dae3955625b8cf006ff89bef3eb9733a76c9ad3e
Vladimir Nikolaev <nikolaevvova20vova03@gmail.com>
2026-08-11 21:58:52 +0200

    add: answer_dataset implemented in pipeline

--- src/app/pipeline.py
+++ src/app/pipeline.py
@@ -1,6 +1,8 @@
 from ..indexing import BM25Indexer, BM25Retriever
 from ..generator import QwenGenerator
+from ..models import (RagDataset, MinimalSearchResults, StudentSearchResults)
 from .catch import catch
+from pathlib import Path
 
 
 class Pipeline:
@@ -22,3 +24,35 @@ class Pipeline:
         results = retriever.search(question, k)
         for r in results:
             print(f"- Found in {r.file_path}")
+
+    @catch
+    def search_dataset(self, dataset_path: str, k: int, save_dir: str) -> None:
+        print(f"Reading {dataset_path}...")
+        with open(dataset_path, "r") as f:
+            json_string = f.read()
+        print(f"Validating the questions...")
+        dataset = RagDataset.model_validate_json(json_string)
+        retriever = BM25Retriever()
+        retriever.load("index.pkl")
+        all_search_results: list[MinimalSearchResults] = []
+        for q in dataset.rag_questions:
+            sources = retriever.search(q.question, k)
+            all_search_results.append(MinimalSearchResults(
+                question_id=q.question_id,
+                question=q.question,
+                retrieved_sources=sources
+            ))
+
+        student_results = StudentSearchResults(
+            search_results=all_search_results,
+            k=k
+        )
+        save_dir_path = Path(save_dir)
+        save_dir_path.mkdir(parents=True, exist_ok=True)
+
+        output_file = save_dir_path / Path(dataset_path).name
+        with open(output_file, "w") as f:
+            f.write(student_results.model_dump_json(indent=2))
+
+        print(f"Successfully saved {len(all_search_results)} results "
+              f"to {output_file}!")
